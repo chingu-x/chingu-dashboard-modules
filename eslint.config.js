33998c49f4fc63b65938418877ce29f60eb6b6bf
Dan Ko <danko56666@gmail.com>
2025-01-15 17:30:29 -0500

    chore: add more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,14 +16,47 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+    },
     plugins: {
       prettier: eslintPluginPrettier,
+      import: importPlugin,
     },
     rules: {
       "prettier/prettier": "error",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+        },
+      ],
+      "no-console": "error",
+      "import/order": 2,
+      "no-nested-ternary": 2,
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
+      "dot-notation": "off",
+      "@typescript-eslint/dot-notation": "error",
     },
   },
   {
-    ignores: ["dist/", "node_modules/**/*"],
+    ignores: [
+      "dist/",
+      "node_modules/**/*",
+      "commitlint.config.mjs",
+      "**/*.config.js",
+      "generate-exports.js",
+      "vite.config.ts",
+      "**/*.test.ts",
+    ],
   },
 ];
