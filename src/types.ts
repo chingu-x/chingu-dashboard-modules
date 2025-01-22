0da3e1a700285c152a648df030c9cea1186b5cb9
Dan Ko <danko56666@gmail.com>
2025-01-21 20:50:23 -0500

    fix: resolve types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,56 @@
+import type { InjectionToken } from "tsyringe";
+import type { RestApiPort } from "./rest-api";
+import type {
+  AuthApiPort,
+  AuthClientAdapter,
+  LoginUsecase,
+  LogoutUsecase,
+  RequestResetPasswordUsecase,
+  ResetPasswordUsecase,
+} from "./auth";
+
 export const TYPES = {
   /* Ports */
-  RestApiPort: Symbol.for("RestApiPort"),
-  AuthApiPort: Symbol.for("AuthApiPort"),
-  UserApiPort: Symbol.for("UserApiPort"),
-  VoyageTeamClientPort: Symbol.for("VoyageTeamClientPort"),
+  RestApiPort: Symbol.for("RestApiPort") as InjectionToken<RestApiPort>,
+  AuthApiPort: Symbol.for("AuthApiPort") as InjectionToken<AuthApiPort>,
+  // UserApiPort: Symbol.for("UserApiPort") as InjectionToken<UserApiPort>,
+  // VoyageTeamClientPort: Symbol.for(
+  //   "VoyageTeamClientPort"
+  // ) as InjectionToken<VoyageTeamClientPort>,
 
   /* Adapters */
-  AuthClientAdapter: Symbol.for("AuthClientAdapter"),
-  UserClientAdapter: Symbol.for("UserClientAdapter"),
-  VoyageTeamClientAdapter: Symbol.for("VoyageTeamClientAdapter"),
+  AuthClientAdapter: Symbol.for(
+    "AuthClientAdapter"
+  ) as InjectionToken<AuthClientAdapter>,
+  // UserClientAdapter: Symbol.for(
+  //   "UserClientAdapter"
+  // ) as InjectionToken<UserClientAdapter>,
+  // VoyageTeamClientAdapter: Symbol.for(
+  //   "VoyageTeamClientAdapter"
+  // ) as InjectionToken<VoyageTeamClientAdapter>,
 
   /* UseCases */
-  LoginUsecase: Symbol.for("LoginUsecase"),
-  LogoutUsecase: Symbol.for("LogoutUsecase"),
-  GetUserUsecase: Symbol.for("GetUserUsecase"),
-  GetChinguMemberStatusUsecase: Symbol.for("GetChinguMemberStatusUsecase"),
-  RequestResetPasswordUsecase: Symbol.for("RequestResetPasswordUsecase"),
-  ResetPasswordUsecase: Symbol.for("ResetPasswordUsecase"),
-  GetCurrentVoyageTeamUsecase: Symbol.for("GetCurrentVoyageTeamUsecase"),
-  GetCurrentVoyageTeamIdUsecase: Symbol.for("GetCurrentVoyageTeamIdUsecase"),
-  HasVoyageStartedUsecase: Symbol.for("HasVoyageStartedUsecase"),
+  LoginUsecase: Symbol.for("LoginUsecase") as InjectionToken<LoginUsecase>,
+  LogoutUsecase: Symbol.for("LogoutUsecase") as InjectionToken<LogoutUsecase>,
+  // GetUserUsecase: Symbol.for(
+  //   "GetUserUsecase"
+  // ) as InjectionToken<GetUserUsecase>,
+  // GetChinguMemberStatusUsecase: Symbol.for(
+  //   "GetChinguMemberStatusUsecase"
+  // ) as InjectionToken<GetChinguMemberStatusUsecase>,
+  RequestResetPasswordUsecase: Symbol.for(
+    "RequestResetPasswordUsecase"
+  ) as InjectionToken<RequestResetPasswordUsecase>,
+  ResetPasswordUsecase: Symbol.for(
+    "ResetPasswordUsecase"
+  ) as InjectionToken<ResetPasswordUsecase>,
+  // GetCurrentVoyageTeamUsecase: Symbol.for(
+  //   "GetCurrentVoyageTeamUsecase"
+  // ) as InjectionToken<GetCurrentVoyageTeamUsecase>,
+  // GetCurrentVoyageTeamIdUsecase: Symbol.for(
+  //   "GetCurrentVoyageTeamIdUsecase"
+  // ) as InjectionToken<GetCurrentVoyageTeamIdUsecase>,
+  // HasVoyageStartedUsecase: Symbol.for(
+  //   "HasVoyageStartedUsecase"
+  // ) as InjectionToken<HasVoyageStartedUsecase>,
 };
