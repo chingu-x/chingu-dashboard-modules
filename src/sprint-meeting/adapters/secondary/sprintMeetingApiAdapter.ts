66fcb2bdb12cef44964d2b740a8f1a58a650d6e0
Dan Ko <danko56666@gmail.com>
2025-01-29 14:33:09 -0500

    feat: implement fetch meeting in sprint meeting api adapter

--- src/sprint-meeting/adapters/secondary/sprintMeetingApiAdapter.ts
+++ src/sprint-meeting/adapters/secondary/sprintMeetingApiAdapter.ts
@@ -2,6 +2,9 @@ import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { SprintMeetingApiPort } from "@/sprint-meeting/ports/secondary/sprintMeetingApiPort";
 import { RestApiPort } from "@/rest-api/ports/secondary/restApiPort";
+import { FetchMeetingResponseDto } from "@/sprint-meeting/application/dtos/response.dto";
+import { FetchMeetingRequestDto } from "@/sprint-meeting/application/dtos/request.dto";
+import SprintMeetingUrls from "@/sprint-meeting/application/constants/sprintMeetingUrls";
 
 @injectable()
 export class SprintMeetingApiAdapter implements SprintMeetingApiPort {
@@ -10,10 +13,11 @@ export class SprintMeetingApiAdapter implements SprintMeetingApiPort {
     private readonly apiClient: RestApiPort
   ) {}
 
-  async login({ email, password }: LoginRequestDto): Promise<LoginResponseDto> {
-    return await this.apiClient.post({
-      url: AuthUrls.login(),
-      payload: { email, password },
+  async fetchMeeting({
+    meetingId,
+  }: FetchMeetingRequestDto): Promise<FetchMeetingResponseDto> {
+    return await this.apiClient.get({
+      url: SprintMeetingUrls.fetchMeeting(meetingId.toString()),
     });
   }
 }
