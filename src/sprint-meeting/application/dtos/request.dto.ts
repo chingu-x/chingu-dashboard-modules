bae91075c87677b6903b2fd4d5507ad1b00f3a78
Dan Ko <danko56666@gmail.com>
2025-01-29 18:01:04 -0500

    feat: add get current sprin meeeting id request dto

--- src/sprint-meeting/application/dtos/request.dto.ts
+++ src/sprint-meeting/application/dtos/request.dto.ts
@@ -1,3 +1,9 @@
+import type { VoyageSprint } from "@/sprints/application/types";
+
 export interface FetchMeetingRequestDto {
   meetingId: number;
+}
+
+export interface GetCurrentSprintMeetingRequesDto {
+  sprints: VoyageSprint;
 }
