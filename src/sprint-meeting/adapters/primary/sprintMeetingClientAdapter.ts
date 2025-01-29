ce1fe534aa63a002e341ad4282504affd64acc11
Dan Ko <danko56666@gmail.com>
2025-01-29 13:50:12 -0500

    feat: implement fetch meeting method in sprint meeting client adapter

--- src/sprint-meeting/adapters/primary/sprintMeetingClientAdapter.ts
+++ src/sprint-meeting/adapters/primary/sprintMeetingClientAdapter.ts
@@ -8,31 +8,18 @@ export class SprintMeetingClientAdapter implements SprintMeetingClientPort {
     @inject(TYPES.LoginUsecase)
     private readonly loginUsecase: LoginUsecase,
 
-    @inject(TYPES.LogoutUsecase)
-    private readonly logoutUsecase: LogoutUsecase,
-
-    @inject(TYPES.RequestResetPasswordUsecase)
-    private readonly requestResetPasswordUsecase: RequestResetPasswordUsecase,
-
-    @inject(TYPES.ResetPasswordUsecase)
-    private readonly resetPasswordUsecase: ResetPasswordUsecase
+    @inject(TYPES.FetchMeetingUsecase)
+    private readonly fetchMeetingUsecase: FetchMeetingUsecase
   ) {}
 
   async login({ email, password }: LoginRequestDto): Promise<LoginResponseDto> {
     return await this.loginUsecase.execute({ email, password });
   }
 
-  async logout(): Promise<LogoutResponseDto> {
-    return await this.logoutUsecase.execute();
-  }
-
-  async requestResetPassword({
-    email,
-  }: RequestResetPasswordDto): Promise<void> {
-    return await this.requestResetPasswordUsecase.execute({ email });
-  }
-
-  async resetPassword({ password, token }: ResetPasswordDto): Promise<void> {
-    return await this.resetPasswordUsecase.execute({ password, token });
+  async fetchMeeting({
+    sprintNumber,
+    meetingId,
+  }: FetchMeetingRequestDto): Promise<FetchMeetingResponseDto> {
+    return await this.fetchMeetingUsecase.execute({ sprintNumber, meetingId });
   }
 }
