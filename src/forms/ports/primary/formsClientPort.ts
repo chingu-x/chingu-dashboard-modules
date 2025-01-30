0375eb59be3cffa79207b08917ef3812388299f9
Dan Ko <danko56666@gmail.com>
2025-01-30 18:58:30 -0500

    chore: update forms client port

--- src/forms/ports/primary/formsClientPort.ts
+++ src/forms/ports/primary/formsClientPort.ts
@@ -1,17 +1,7 @@
-import type {
-  FetchFormQuestionsRequestDto,
-  GetWeeklyCheckinFormRequestDto,
-} from "@/forms/application/dtos/request.dto";
-import type {
-  FetchFormQuestionsResponseDto,
-  GetWeeklyCheckinFormResponseDto,
-} from "@/forms/application/dtos/response.dto";
+import type { GetWeeklyCheckinFormRequestDto } from "@/forms/application/dtos/request.dto";
+import type { GetWeeklyCheckinFormResponseDto } from "@/forms/application/dtos/response.dto";
 
 export interface FormsClientPort {
-  fetchFormQuestions: (
-    props: FetchFormQuestionsRequestDto
-  ) => Promise<FetchFormQuestionsResponseDto>;
-
   getWeeklyCheckinForm: (
     props: GetWeeklyCheckinFormRequestDto
   ) => Promise<GetWeeklyCheckinFormResponseDto>;
