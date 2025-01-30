b0afd24dfd338021ef415e7ac57625c4c7fe591b
Dan Ko <danko56666@gmail.com>
2025-01-30 16:45:50 -0500

    feat: implement fetch form questions usecase

--- src/forms/adapters/secondary/formsApiAdapter.ts
+++ src/forms/adapters/secondary/formsApiAdapter.ts
@@ -4,6 +4,7 @@ import { RestApiPort } from "@/rest-api/ports/secondary/restApiPort";
 import { FetchFormQuestionsRequestDto } from "@/forms/application/dtos/request.dto";
 import { FetchFormQuestionsResponseDto } from "@/forms/application/dtos/response.dto";
 import FormsUrls from "@/forms/application/constants/formsUrls";
+import { FormsApiPort } from "@/forms/ports/secondary/formApiPort";
 
 @injectable()
 export class FormsApiAdapter implements FormsApiPort {
---
+++ src/forms/application/usecases/fetchFormQuestionsUsecase.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from "tsyringe";
+import { FormsApiPort } from "@/forms/ports/secondary/formApiPort";
+import { FetchFormQuestionsRequestDto } from "@/forms/application/dtos/request.dto";
+import { FetchFormQuestionsResponseDto } from "@/forms/application/dtos/response.dto";
+
+@injectable()
+export class LoginUsecase {
+  constructor(
+    @inject(TYPES.FormsApiPort)
+    private readonly formsApi: FormsApiPort
+  ) {}
+
+  async execute(
+    props: FetchFormQuestionsRequestDto
+  ): Promise<FetchFormQuestionsResponseDto> {
+    return await this.formsApi.fetchFormQuestions({
+      ...props,
+    });
+  }
+}
