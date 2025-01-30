f9bfa377abf43378b1d71a0feb9af6be6c089623
Dan Ko <danko56666@gmail.com>
2025-01-30 16:50:25 -0500

    feat: setup form dependencies in form di container

--- src/forms/di/config.ts
+++ src/forms/di/config.ts
@@ -1,22 +1,20 @@
 import { container } from "tsyringe";
+import { FORMS_TYPES } from "./types";
 import { FormsApiAdapter } from "@/forms/adapters/secondary/formsApiAdapter";
+import { FormsClientAdapter } from "@/forms/adapters/primary/formsClientAdapter";
+import { FetchFormQuestionsUsecase } from "@/forms/application/usecases/fetchFormQuestionsUsecase";
 
 export const registerFormsDependencies = () => {
   // ports
   container.register(FORMS_TYPES.FormsApiPort, { useClass: FormsApiAdapter });
 
   // adapters
-  container.register(AUTH_TYPES.AuthClientAdapter, {
-    useClass: AuthClientAdapter,
+  container.register(FORMS_TYPES.FormsClientAdapter, {
+    useClass: FormsClientAdapter,
   });
 
   // usecases
-  container.register(AUTH_TYPES.LoginUsecase, { useClass: LoginUsecase });
-  container.register(AUTH_TYPES.LogoutUsecase, { useClass: LogoutUsecase });
-  container.register(AUTH_TYPES.RequestResetPasswordUsecase, {
-    useClass: RequestResetPasswordUsecase,
-  });
-  container.register(AUTH_TYPES.ResetPasswordUsecase, {
-    useClass: ResetPasswordUsecase,
+  container.register(FORMS_TYPES.FetchFormQuestionsUsecase, {
+    useClass: FetchFormQuestionsUsecase,
   });
 };
