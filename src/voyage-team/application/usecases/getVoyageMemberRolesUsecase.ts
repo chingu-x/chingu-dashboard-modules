17e2196de90d706a1dce18f8a8673367448f8389
Dan Ko <danko56666@gmail.com>
2025-01-30 17:33:31 -0500

    feat: implement get voyage member roles usecase

---
+++ src/voyage-team/application/usecases/getVoyageMemberRolesUsecase.ts
@@ -0,0 +1,28 @@
+import { injectable } from "tsyringe";
+import { GetVoyageMemberRolesRequestDto } from "@/voyage-team/application/dtos/request.dto";
+import { GetVoyageMemberRolesResponseDto } from "@/voyage-team/application/dtos/response.dto";
+import { UserRole } from "@/forms/application/types";
+
+@injectable()
+export class GetVoyageMemberRolesUsecase {
+  execute({
+    voyageTeam,
+  }: GetVoyageMemberRolesRequestDto): GetVoyageMemberRolesResponseDto {
+    const hasScrumMaster = Boolean(
+      voyageTeam.voyageTeamMembers.find(
+        (member) => member.voyageRole.name === UserRole.scrumMaster.toString()
+      )
+    );
+
+    const hasProductOwner = Boolean(
+      voyageTeam.voyageTeamMembers.find(
+        (member) => member.voyageRole.name === UserRole.productOwner.toString()
+      )
+    );
+
+    return {
+      hasScrumMaster,
+      hasProductOwner,
+    };
+  }
+}
