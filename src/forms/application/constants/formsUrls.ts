b4bb845a8b9e3be5566255531550244462f0258b
Dan Ko <danko56666@gmail.com>
2025-01-31 18:04:27 -0500

    feat: add submit weekly check in form url

--- src/forms/application/constants/formsUrls.ts
+++ src/forms/application/constants/formsUrls.ts
@@ -2,6 +2,9 @@ const FormsUrls = {
   fetchFormQuestions(formId: number) {
     return `/api/v1/forms/${formId}`;
   },
+  submitWeeklyCheckinForm() {
+    return "/api/v1/voyages/sprints/check-in";
+  },
 } as const;
 
 export default FormsUrls;
