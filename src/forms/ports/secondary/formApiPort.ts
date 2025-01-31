483a408bb2e7fe98430450950225e84ab0f8088a
Dan Ko <danko56666@gmail.com>
2025-01-31 18:07:15 -0500

    feat: add submit weekly checkin form in forms api port

--- src/forms/ports/secondary/formApiPort.ts
+++ src/forms/ports/secondary/formApiPort.ts
@@ -1,8 +1,20 @@
-import type { FetchFormQuestionsRequestDto } from "@/forms/application/dtos/request.dto";
-import type { FetchFormQuestionsResponseDto } from "@/forms/application/dtos/response.dto";
+import type {
+  FetchFormQuestionsRequestDto,
+  SubmitWeeklyCheckinFormRequestDto,
+} from "@/forms/application/dtos/request.dto";
+import type {
+  FetchFormQuestionsResponseDto,
+  SubmitWeeklyCheckinFormResponseDto,
+} from "@/forms/application/dtos/response.dto";
 
 export interface FormsApiPort {
   fetchFormQuestions: ({
     formId,
   }: FetchFormQuestionsRequestDto) => Promise<FetchFormQuestionsResponseDto>;
+
+  submitWeeklyCheckinForm: ({
+    voyageTeamMemberId,
+    sprintId,
+    responses,
+  }: SubmitWeeklyCheckinFormRequestDto) => Promise<SubmitWeeklyCheckinFormResponseDto>;
 }
