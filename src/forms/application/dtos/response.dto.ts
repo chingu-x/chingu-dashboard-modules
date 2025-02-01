75a52de6c21c64968e77af95d972258b0e43fde7
Dan Ko <danko56666@gmail.com>
2025-02-01 16:17:20 -0500

    feat: add fetch submit voyage project form response dto

--- src/forms/application/dtos/response.dto.ts
+++ src/forms/application/dtos/response.dto.ts
@@ -3,6 +3,8 @@ import type { FormQuestions } from "@/forms/application/types";
 export type FetchFormQuestionsResponseDto = FormQuestions;
 
 export type FetchWeeklyCheckinFormResponseDto = FetchFormQuestionsResponseDto;
+export type FetchSubmitVoyageProjectFormRequestDto =
+  FetchFormQuestionsResponseDto;
 
 export interface SubmitWeeklyCheckinFormResponseDto {
   id: number;
