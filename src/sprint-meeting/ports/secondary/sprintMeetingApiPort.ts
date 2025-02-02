3a53cc15a2ee36597a776bf0ad94b42b7f775d14
Dan Ko <danko56666@gmail.com>
2025-02-01 21:55:02 -0500

    add meeting function in sprint meeting api port

--- src/sprint-meeting/ports/secondary/sprintMeetingApiPort.ts
+++ src/sprint-meeting/ports/secondary/sprintMeetingApiPort.ts
@@ -1,8 +1,22 @@
-import type { FetchMeetingRequestDto } from "@/sprint-meeting/application/dtos/request.dto";
-import type { FetchMeetingResponseDto } from "@/sprint-meeting/application/dtos/response.dto";
+import type {
+  AddMeetingApiRequestDto,
+  FetchMeetingRequestDto,
+} from "@/sprint-meeting/application/dtos/request.dto";
+import type {
+  AddMeetingResponseDto,
+  FetchMeetingResponseDto,
+} from "@/sprint-meeting/application/dtos/response.dto";
 
 export interface SprintMeetingApiPort {
   fetchMeeting: ({
     meetingId,
   }: FetchMeetingRequestDto) => Promise<FetchMeetingResponseDto>;
+  addMeeting: ({
+    teamId,
+    sprintNumber,
+    title,
+    description,
+    dateTime,
+    meetingLink,
+  }: AddMeetingApiRequestDto) => Promise<AddMeetingResponseDto>;
 }
