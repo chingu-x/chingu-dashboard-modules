39f5cd2be50df5cd99498ca83c0e8a007a76f615
Dan Ko <danko56666@gmail.com>
2025-02-02 22:43:47 -0500

    chore: add UpdateDirectoryWithCurrentTimeUsecaseResponseDto

--- src/timezone/application/usecases/updateDirectoryWithCurrentTimeUsecase.ts
+++ src/timezone/application/usecases/updateDirectoryWithCurrentTimeUsecase.ts
@@ -1,10 +1,13 @@
 import { injectable } from "tsyringe";
 import { UpdateDirectoryWithCurrentTimeUsecaseRequestDto } from "@/timezone/application/dtos/request.dto";
 import { getTimezone } from "@/timezone/application/utils/getTimezone";
+import { UpdateDirectoryWithCurrentTimeUsecaseResponseDto } from "@/timezone/application/dtos/response.dto";
 
 @injectable()
 export class UpdateDirectoryWithCurrentTimeUsecase {
-  execute({ data }: UpdateDirectoryWithCurrentTimeUsecaseRequestDto): void {
+  execute({
+    data,
+  }: UpdateDirectoryWithCurrentTimeUsecaseRequestDto): UpdateDirectoryWithCurrentTimeUsecaseResponseDto {
     return data.voyageTeamMembers.forEach((teamMember) => {
       const { timezone } = teamMember.member;
       const currentTime = getTimezone(timezone);
