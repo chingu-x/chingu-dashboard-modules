4249f19ed7ef721f16132fc9ace61725b270514e
Dan Ko <danko56666@gmail.com>
2025-02-03 23:24:16 -0500

    chore: update timezone di container

--- src/timezone/di/config.ts
+++ src/timezone/di/config.ts
@@ -1,14 +1,23 @@
 import { container } from "tsyringe";
 import { TIME_ZONE_TYPES } from "./types";
 import { UpdateDirectoryWithCurrentTimeUsecase } from "@/timezone/application/usecases/updateDirectoryWithCurrentTimeUsecase";
+import { GetMeetingLongDateTimeFormatUsecase } from "@/timezone/application/usecases/getMeetingLongDateTimeFormatUsecase";
+import { TimezoneClientAdapter } from "@/timezone/adapters/primary/timezoneClientAdapter";
 
 export const registerTimezoneDependencies = () => {
   // ports
 
-  // adapters
-
   // usecases
   container.register(TIME_ZONE_TYPES.UpdateDirectoryWithCurrentTimeUsecase, {
     useClass: UpdateDirectoryWithCurrentTimeUsecase,
+  });
+
+  container.register(TIME_ZONE_TYPES.GetMeetingLongDateTimeFormatUsecase, {
+    useClass: GetMeetingLongDateTimeFormatUsecase,
+  });
+
+  // adapters
+  container.register(TIME_ZONE_TYPES.TimezoneClientAdapter, {
+    useClass: TimezoneClientAdapter,
   });
 };
--- src/timezone/di/types.ts
+++ src/timezone/di/types.ts
@@ -1,11 +1,10 @@
 import type { InjectionToken } from "tsyringe";
 import type { UpdateDirectoryWithCurrentTimeUsecase } from "@/timezone/application/usecases/updateDirectoryWithCurrentTimeUsecase";
 import type { GetMeetingLongDateTimeFormatUsecase } from "@/timezone/application/usecases/getMeetingLongDateTimeFormatUsecase";
+import type { TimezoneClientAdapter } from "@/timezone/adapters/primary/timezoneClientAdapter";
 
 export const TIME_ZONE_TYPES = {
   // ports
-
-  // adapters
 
   // usecases
   UpdateDirectoryWithCurrentTimeUsecase: Symbol.for(
@@ -15,4 +14,9 @@ export const TIME_ZONE_TYPES = {
   GetMeetingLongDateTimeFormatUsecase: Symbol.for(
     "GetMeetingLongDateTimeFormatUsecase",
   ) as InjectionToken<GetMeetingLongDateTimeFormatUsecase>,
+
+  // adapters
+  TimezoneClientAdapter: Symbol.for(
+    "TimezoneClientAdapter",
+  ) as InjectionToken<TimezoneClientAdapter>,
 };
