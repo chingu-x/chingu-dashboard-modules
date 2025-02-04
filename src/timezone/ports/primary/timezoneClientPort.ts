cf31413fc067b92c0ffb1fd88e68d5e35a45881a
Dan Ko <danko56666@gmail.com>
2025-02-03 22:29:58 -0500

    chore: update imports

--- src/timezone/ports/primary/timezoneClientPort.ts
+++ src/timezone/ports/primary/timezoneClientPort.ts
@@ -1,3 +1,6 @@
+import type { GetMeetingLongDateTimeFormatClientRequestDto } from "@/timezone/application/dtos/request.dto";
+import type { GetMeetingLongDateTimeFormatResponseDto } from "@/timezone/application/dtos/response.dto";
+
 export interface TimezoneClientPort {
   getMeetingLongDateTimeFormat: (
     props: GetMeetingLongDateTimeFormatClientRequestDto,
