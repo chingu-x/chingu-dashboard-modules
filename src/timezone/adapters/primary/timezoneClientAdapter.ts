b583926e4ba74ee52df515cc2f18d55231d34ce7
Dan Ko <danko56666@gmail.com>
2025-02-04 19:33:26 -0500

    feat: implement get meeting date function in timezone adapter

--- src/timezone/adapters/primary/timezoneClientAdapter.ts
+++ src/timezone/adapters/primary/timezoneClientAdapter.ts
@@ -10,6 +10,9 @@ export class TimezoneClientAdapter implements TimezoneClientPort {
   constructor(
     @inject(TYPES.GetMeetingLongDateTimeFormatUsecase)
     private readonly getMeetingLongDateTimeFormatUsecase: GetMeetingLongDateTimeFormatUsecase,
+
+    @inject(TYPES.GetMeetingDatetUsecase)
+    private readonly getMeetingDateUsecase: GetMeetingDateUsecase,
   ) {}
 
   // returns date in this format as a date: October 10, 2024 12:00 AM in the user's timezone
@@ -19,6 +22,19 @@ export class TimezoneClientAdapter implements TimezoneClientPort {
   }: GetMeetingLongDateTimeFormatClientRequestDto): GetMeetingLongDateTimeFormatResponseDto {
     return this.getMeetingLongDateTimeFormatUsecase.execute({
       meetingDateTime,
+      timezone,
+    });
+  }
+
+  // returns date of when the meeting is
+  // if meeting is today it will return "today"
+  // if meeting is tomorrow it will return "tomorrow" etc
+  getMeetingDate({
+    dateTime,
+    timezone,
+  }: GetMeetingDateClientRequestDto): GetMeetingDateResponseDto {
+    return this.getMeetingDateUsecase.execute({
+      dateTime,
       timezone,
     });
   }
