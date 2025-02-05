5aa0d4f65dc2318bfa31cca1f896a13786c5dcd3
Dan Ko <danko56666@gmail.com>
2025-02-04 22:49:15 -0500

    feat: implement EditAgendaTopicUsecase

---
+++ src/sprint-meeting/application/usecases/editAgendaTopicUsecase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from "tsyringe";
+import { SPRINT_MEETING_TYPES } from "@/sprint-meeting/di/types";
+import { SprintMeetingApiPort } from "@/sprint-meeting/ports/secondary/sprintMeetingApiPort";
+import { EditAgendaTopicUsecaseDto } from "@/sprint-meeting/application/dtos/usecase.dto";
+import { EditAgendaTopicResponseDto } from "@/sprint-meeting/application/dtos/response.dto";
+
+@injectable()
+export class EditAgendaTopicUsecase {
+  constructor(
+    @inject(SPRINT_MEETING_TYPES.SprintMeetingApiPort)
+    private readonly sprintMeetingApi: SprintMeetingApiPort,
+  ) {}
+
+  async execute({
+    agendaId,
+    title,
+    description,
+    status,
+  }: EditAgendaTopicUsecaseDto): Promise<EditAgendaTopicResponseDto> {
+    return await this.sprintMeetingApi.editAgendaTopic({
+      agendaId,
+      title,
+      description,
+      status,
+    });
+  }
+}
