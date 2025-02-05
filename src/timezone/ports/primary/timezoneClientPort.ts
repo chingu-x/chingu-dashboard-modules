7583414af8a4159a483ad81e0f7999fb0ba3a7a0
Dan Ko <danko56666@gmail.com>
2025-02-04 19:37:15 -0500

    feat: add get meeting date function in timezone client port

--- src/timezone/ports/primary/timezoneClientPort.ts
+++ src/timezone/ports/primary/timezoneClientPort.ts
@@ -1,8 +1,18 @@
-import type { GetMeetingLongDateTimeFormatClientRequestDto } from "@/timezone/application/dtos/request.dto";
-import type { GetMeetingLongDateTimeFormatResponseDto } from "@/timezone/application/dtos/response.dto";
+import type {
+  GetMeetingDateClientRequestDto,
+  GetMeetingLongDateTimeFormatClientRequestDto,
+} from "@/timezone/application/dtos/request.dto";
+import type {
+  GetMeetingDateResponseDto,
+  GetMeetingLongDateTimeFormatResponseDto,
+} from "@/timezone/application/dtos/response.dto";
 
 export interface TimezoneClientPort {
   getMeetingLongDateTimeFormat: (
     props: GetMeetingLongDateTimeFormatClientRequestDto,
   ) => GetMeetingLongDateTimeFormatResponseDto;
+
+  getMeetingDate: (
+    props: GetMeetingDateClientRequestDto,
+  ) => GetMeetingDateResponseDto;
 }
