e308adc66ed90c6d909f7d4a2b008b474f0e63a3
Dan Ko <danko56666@gmail.com>
2025-03-10 21:53:01 -0400

    chore: add GetActiveTeamsResponseDto

--- src/user/application/dtos/response.dto.ts
+++ src/user/application/dtos/response.dto.ts
@@ -1,5 +1,8 @@
 import { type User } from "@/user/application/types";
+import type { UserVoyageTeamMember } from "@/voyage-team/application/types";
 
 export type FetchUserResponseDto = User;
 
 export type GetChinguMemberStatusResponseDto = boolean;
+
+export type GetActiveTeamsResponseDto = UserVoyageTeamMember[];
