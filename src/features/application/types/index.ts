3611a863a7479366a99d8c65ac8dda822c19b383
Dan Ko <danko56666@gmail.com>
2025-03-16 23:09:29 -0400

    chore: add features types

---
+++ src/features/application/types/index.ts
@@ -0,0 +1,30 @@
+interface Category {
+  id: number;
+  name: string;
+}
+
+export interface VoyageMember {
+  id: string;
+  avatar: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface Features {
+  id: number;
+  description: string;
+  order: number;
+  createdAt: Date;
+  updatedAt: Date;
+  teamMemberId: number;
+  category: Category;
+  addedBy: {
+    member: VoyageMember;
+  };
+}
+
+export interface FeaturesList {
+  categoryId: number;
+  categoryName: string;
+  features: Features[];
+}
