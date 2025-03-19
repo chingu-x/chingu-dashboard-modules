f84b4396067f4df5452dbc76ed8ab69097849f44
Dan Ko <danko56666@gmail.com>
2025-03-18 23:57:15 -0400

    chore: update imports

--- src/features/ports/primary/featuresClientPort.ts
+++ src/features/ports/primary/featuresClientPort.ts
@@ -2,13 +2,16 @@ import type {
   AddFeatureClientRequestDto,
   FetchFeaturesClientRequestDto,
 } from "@/features/application/dtos/request.dto";
-import type { FetchFeaturesClientResponseDto } from "@/features/application/dtos/response.dto";
-
-type NewType = AddFeatureResponseDto;
+import type {
+  AddFeatureClientResponseDto,
+  FetchFeaturesClientResponseDto,
+} from "@/features/application/dtos/response.dto";
 
 export interface FeaturesClientPort {
   fetchFeatures: (
     props: FetchFeaturesClientRequestDto,
   ) => Promise<FetchFeaturesClientResponseDto>;
-  addFeature: (props: AddFeatureClientRequestDto) => Promise<NewType>;
+  addFeature: (
+    props: AddFeatureClientRequestDto,
+  ) => Promise<AddFeatureClientResponseDto>;
 }
