b17a61da54ca225684e42e9033e5f28cc93f0168
Dan Ko <danko56666@gmail.com>
2025-03-19 00:04:49 -0400

    feat: implement addFeature function in features api adapter

--- src/features/adapters/secondary/featuresApiAdapter.ts
+++ src/features/adapters/secondary/featuresApiAdapter.ts
@@ -2,8 +2,14 @@ import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { RestApiPort } from "@/rest-api/ports/secondary/restApiPort";
 import { FeaturesApiPort } from "@/features/ports/secondary/featuresApiPort";
-import { FetchFeaturesApiRequestDto } from "@/features/application/dtos/request.dto";
-import { FetchFeaturesApiResponseDto } from "@/features/application/dtos/response.dto";
+import {
+  AddFeatureApiRequestDto,
+  FetchFeaturesApiRequestDto,
+} from "@/features/application/dtos/request.dto";
+import {
+  AddFeatureApiResponseDto,
+  FetchFeaturesApiResponseDto,
+} from "@/features/application/dtos/response.dto";
 import FeaturesUrls from "@/features/application/constants/featuresUrls";
 
 @injectable()
@@ -18,6 +24,17 @@ export class FeaturesApiAdapter implements FeaturesApiPort {
   }: FetchFeaturesApiRequestDto): Promise<FetchFeaturesApiResponseDto> {
     return await this.apiClient.get({
       url: FeaturesUrls.fetchFeatures({ teamId }),
+    });
+  }
+
+  async addFeature({
+    teamId,
+    description,
+    featureCategoryId,
+  }: AddFeatureApiRequestDto): Promise<AddFeatureApiResponseDto> {
+    return await this.apiClient.post({
+      url: FeaturesUrls.addFeature({ teamId }),
+      payload: { description, featureCategoryId },
     });
   }
 }
