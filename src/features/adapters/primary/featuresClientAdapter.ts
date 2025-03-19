cc9f2b40683a44cf2a2cc73f7ab4f3336297c7f3
Dan Ko <danko56666@gmail.com>
2025-03-19 00:00:16 -0400

    feat: imlement addFeature function in features client adapter

--- src/features/adapters/primary/featuresClientAdapter.ts
+++ src/features/adapters/primary/featuresClientAdapter.ts
@@ -1,8 +1,14 @@
 import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { FeaturesClientPort } from "@/features/ports/primary/featuresClientPort";
-import { FetchFeaturesClientRequestDto } from "@/features/application/dtos/request.dto";
-import { FetchFeaturesClientResponseDto } from "@/features/application/dtos/response.dto";
+import {
+  AddFeatureClientRequestDto,
+  FetchFeaturesClientRequestDto,
+} from "@/features/application/dtos/request.dto";
+import {
+  AddFeatureClientResponseDto,
+  FetchFeaturesClientResponseDto,
+} from "@/features/application/dtos/response.dto";
 import { FetchFeaturesUsecase } from "@/features/application/usecases/fetchFeaturesUsecase";
 
 @injectable()
@@ -10,11 +16,26 @@ export class FeaturesClientAdapter implements FeaturesClientPort {
   constructor(
     @inject(TYPES.FetchFeaturesUsecase)
     private readonly fetchFeaturesUsecase: FetchFeaturesUsecase,
+
+    @inject(TYPES.AddFeatureUsecase)
+    private readonly addFeatureUsecase: AddFeatureUsecase,
   ) {}
 
-  fetchFeatures({
+  async fetchFeatures({
     teamId,
   }: FetchFeaturesClientRequestDto): Promise<FetchFeaturesClientResponseDto> {
-    return this.fetchFeaturesUsecase.execute({ teamId });
+    return await this.fetchFeaturesUsecase.execute({ teamId });
+  }
+
+  async addFeature({
+    teamId,
+    description,
+    featureCategoryId,
+  }: AddFeatureClientRequestDto): Promise<AddFeatureClientResponseDto> {
+    return await this.addFeatureUsecase.execute({
+      teamId,
+      description,
+      featureCategoryId,
+    });
   }
 }
