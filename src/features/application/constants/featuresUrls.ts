9643abd19927c0eda6f2b1282bfceae4c76679ab
Dan Ko <danko56666@gmail.com>
2025-03-19 00:02:48 -0400

    chore: add addFeature url

--- src/features/application/constants/featuresUrls.ts
+++ src/features/application/constants/featuresUrls.ts
@@ -2,8 +2,15 @@ interface FetchFeaturesUrlProps {
   teamId: string;
 }
 
+interface AddFeatureUrlProps {
+  teamId: string;
+}
+
 const FeaturesUrls = {
   fetchFeatures({ teamId }: FetchFeaturesUrlProps) {
+    return `api/v1/voyages/teams/${teamId}/features`;
+  },
+  addFeature({ teamId }: AddFeatureUrlProps) {
     return `api/v1/voyages/teams/${teamId}/features`;
   },
 } as const;
