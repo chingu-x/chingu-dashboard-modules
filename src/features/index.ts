9c60e2b1efc59a098b0c12799c4743d20263d6a5
Dan Ko <danko56666@gmail.com>
2025-03-19 23:20:20 -0400

    chore: update exports

--- src/features/index.ts
+++ src/features/index.ts
@@ -2,9 +2,11 @@ export { FeaturesClientAdapter } from "./adapters/primary/featuresClientAdapter"
 export {
   AddFeatureClientRequestDto,
   EditFeatureClientRequestDto,
+  DeleteFeatureClientRequestDto,
 } from "./application/dtos/request.dto";
 export {
   AddFeatureClientResponseDto,
   EditFeatureClientResponseDto,
+  DeleteFeatureClientResponseDto,
 } from "./application/dtos/response.dto";
 export { VoyageMember, Features, FeaturesList } from "./application/types";
