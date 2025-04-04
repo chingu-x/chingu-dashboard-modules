720f430568d25ac5ba9fd16f4253f4e38a239249
Dan Ko <danko56666@gmail.com>
2025-04-03 23:38:39 -0400

    feat: implement addVoyageResource in VoyageResourcesApiAdapter

--- src/voyage-resources/adapters/secondary/voyage-resources-api-adapter.ts
+++ src/voyage-resources/adapters/secondary/voyage-resources-api-adapter.ts
@@ -2,8 +2,14 @@ import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { RestApiPort } from "@/rest-api/ports/secondary/rest-api-port";
 import { VoyageResourcesApiPort } from "@/voyage-resources/ports/secondary/voyage-resources-api-port";
-import { FetchVoyageResourcesApiRequestDto } from "@/voyage-resources/application/dtos/request.dto";
-import { FetchVoyageResourcesResponseDto } from "@/voyage-resources/application/dtos/response.dto";
+import {
+  AddVoyageResourceApiRequestDto,
+  FetchVoyageResourcesApiRequestDto,
+} from "@/voyage-resources/application/dtos/request.dto";
+import {
+  AddVoyageResourceResponseDto,
+  FetchVoyageResourcesResponseDto,
+} from "@/voyage-resources/application/dtos/response.dto";
 import VoyageResourcesUrls from "@/voyage-resources/application/constants/voyage-resources-urls";
 
 @injectable()
@@ -18,6 +24,17 @@ export class VoyageResourcesApiAdapter implements VoyageResourcesApiPort {
   }: FetchVoyageResourcesApiRequestDto): Promise<FetchVoyageResourcesResponseDto> {
     return await this.apiClient.get({
       url: VoyageResourcesUrls.fetchVoyageResources({ teamId }),
+    });
+  }
+
+  async addVoyageResource({
+    teamId,
+    url,
+    title,
+  }: AddVoyageResourceApiRequestDto): Promise<AddVoyageResourceResponseDto> {
+    return await this.apiClient.post({
+      url: VoyageResourcesUrls.addVoyageResource({ teamId }),
+      payload: { url, title },
     });
   }
 }
