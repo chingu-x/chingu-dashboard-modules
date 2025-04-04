a9f6a04c208c775ba3a9fbeb650166eaa9482905
Dan Ko <danko56666@gmail.com>
2025-04-03 23:43:49 -0400

    chore: update exports

--- src/voyage-resources/index.ts
+++ src/voyage-resources/index.ts
@@ -1,3 +1,7 @@
 export { VoyageResourcesClientAdapter } from "./adapters/primary/voyage-resources-client-adapter";
-export { FetchVoyageResourcesClientRequestDto } from "./application/dtos/request.dto";
+export {
+  FetchVoyageResourcesClientRequestDto,
+  AddVoyageResourceClientRequestDto,
+} from "./application/dtos/request.dto";
+export { AddVoyageResourceResponseDto } from "./application/dtos/response.dto";
 export { VoyageResource } from "./application/types/index";
