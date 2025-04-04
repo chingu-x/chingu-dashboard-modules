0a8a198440e7d092dce1bfb983034260ac2bdb80
Dan Ko <danko56666@gmail.com>
2025-04-03 23:33:52 -0400

    chore: update imports

--- src/voyage-resources/ports/secondary/voyage-resources-api-port.ts
+++ src/voyage-resources/ports/secondary/voyage-resources-api-port.ts
@@ -1,10 +1,20 @@
-import type { FetchVoyageResourcesApiRequestDto } from "@/voyage-resources/application/dtos/request.dto";
-import type { FetchVoyageResourcesResponseDto } from "@/voyage-resources/application/dtos/response.dto";
+import type {
+  AddVoyageResourceApiRequestDto,
+  FetchVoyageResourcesApiRequestDto,
+} from "@/voyage-resources/application/dtos/request.dto";
+import type {
+  AddVoyageResourceResponseDto,
+  FetchVoyageResourcesResponseDto,
+} from "@/voyage-resources/application/dtos/response.dto";
 
 export interface VoyageResourcesApiPort {
   fetchVoyageResources: ({
     teamId,
   }: FetchVoyageResourcesApiRequestDto) => Promise<FetchVoyageResourcesResponseDto>;
 
-  addVoyageResource({ teamId, url, title }: AddVoyageResourceApiRequestDto) => Promise<AddVoyageResourceResponseDto>
+  addVoyageResource: ({
+    teamId,
+    url,
+    title,
+  }: AddVoyageResourceApiRequestDto) => Promise<AddVoyageResourceResponseDto>;
 }
