ae14c2068cd68b292e5bbefe3ba00b873a9bbcad
Dan Ko <danko56666@gmail.com>
2025-04-03 23:54:42 -0400

    chore: add deleteVoyageResource url

--- src/voyage-resources/application/constants/voyage-resources-urls.ts
+++ src/voyage-resources/application/constants/voyage-resources-urls.ts
@@ -6,12 +6,19 @@ interface AddVoyageResourceProps {
   teamId: string;
 }
 
+interface DeleteVoyageResourceProps {
+  resourceId: number;
+}
+
 const VoyageResourcesUrls = {
   fetchVoyageResources({ teamId }: FetchVoyageResourcesProps) {
     return `api/v1/voyages/teams/${teamId}`;
   },
   addVoyageResource({ teamId }: AddVoyageResourceProps) {
     return `api/v1/voyages/teams/${teamId}`;
+  },
+  deleteVoyageResource({ resourceId }: DeleteVoyageResourceProps) {
+    return `api/v1/voyages/resources/${resourceId}`;
   },
 } as const;
 
