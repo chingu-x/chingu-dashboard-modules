5ea7c93c13b2bbf58ceec43a6475305f4040a8a6
Dan Ko <danko56666@gmail.com>
2025-04-03 23:13:14 -0400

    feat: implement FetchVoyageResourcesUsecase

--- src/voyage-resources/application/usecases/fetch-voyage-resources-usecase.ts
+++ src/voyage-resources/application/usecases/fetch-voyage-resources-usecase.ts
@@ -1,16 +1,19 @@
 import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
+import { VoyageResourcesApiPort } from "@/voyage-resources/ports/secondary/voyage-resources-api-port";
+import { FetchVoyageResourcesUsecaseDto } from "@/voyage-resources/application/dtos/usecase.dto";
+import { FetchVoyageResourcesResponseDto } from "@/voyage-resources/application/dtos/response.dto";
 
 @injectable()
 export class FetchVoyageResourcesUsecase {
   constructor(
-    @inject(TYPES.AuthApiPort)
-    private readonly authApi: AuthApiPort,
+    @inject(TYPES.VoyageResourcesApiPort)
+    private readonly voyageResourcesApi: VoyageResourcesApiPort,
   ) {}
 
-  async execute(props: LoginUsecaseDto): Promise<LoginResponseDto> {
-    return await this.authApi.login({
-      ...props,
-    });
+  async execute(
+    props: FetchVoyageResourcesUsecaseDto,
+  ): Promise<FetchVoyageResourcesResponseDto> {
+    return await this.voyageResourcesApi.fetchVoyageResources(props);
   }
 }
