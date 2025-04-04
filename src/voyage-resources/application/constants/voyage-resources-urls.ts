495ae1ef1058bec06b098deb44b28110de2ada52
Dan Ko <danko56666@gmail.com>
2025-04-03 23:37:16 -0400

    chore: add addVoyageResource url

--- src/voyage-resources/application/constants/voyage-resources-urls.ts
+++ src/voyage-resources/application/constants/voyage-resources-urls.ts
@@ -2,8 +2,15 @@ interface FetchVoyageResourcesProps {
   teamId: string;
 }
 
+interface AddVoyageResourceProps {
+  teamId: string;
+}
+
 const VoyageResourcesUrls = {
   fetchVoyageResources({ teamId }: FetchVoyageResourcesProps) {
+    return `api/v1/voyages/teams/${teamId}`;
+  },
+  addVoyageResource({ teamId }: AddVoyageResourceProps) {
     return `api/v1/voyages/teams/${teamId}`;
   },
 } as const;
