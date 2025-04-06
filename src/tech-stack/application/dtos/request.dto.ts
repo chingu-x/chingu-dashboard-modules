034772e388625de9dfcf98810b9201cd5b609fd0
Dan Ko <danko56666@gmail.com>
2025-04-06 00:14:07 -0400

    chore: add DeleteTechStackItemApiRequestDto

--- src/tech-stack/application/dtos/request.dto.ts
+++ src/tech-stack/application/dtos/request.dto.ts
@@ -35,3 +35,7 @@ export interface EditTechStackItemApiRequestDto {
   teamTechItemId: number;
   techName: string;
 }
+
+export interface DeleteTechStackItemApiRequestDto {
+  teamTechItemId: number;
+}
