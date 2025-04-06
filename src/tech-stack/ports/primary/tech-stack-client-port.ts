89f55dc452b4ba868c12a5e1dbed16bf2478c12b
Dan Ko <danko56666@gmail.com>
2025-04-05 23:02:15 -0400

    chore: update imports

--- src/tech-stack/ports/primary/tech-stack-client-port.ts
+++ src/tech-stack/ports/primary/tech-stack-client-port.ts
@@ -1,3 +1,6 @@
+import type { FetchTechStackClientRequestDto } from "@/tech-stack/application/dtos/request.dto";
+import type { FetchTechStackResponseDto } from "@/tech-stack/application/dtos/response.dto";
+
 export interface TechStackClientPort {
   fetchTechStack: (
     props: FetchTechStackClientRequestDto,
