6d5ada23cc2785c54303e5b2db39c9986d4d9c4e
Dan Ko <danko56666@gmail.com>
2025-04-05 23:37:10 -0400

    chore: add AddTechStackItemUsecaseDto

--- src/tech-stack/application/dtos/usecase.dto.ts
+++ src/tech-stack/application/dtos/usecase.dto.ts
@@ -1,3 +1,9 @@
 export interface FetchTechStackUsecaseDto {
   teamId: string;
 }
+
+export interface AddTechStackItemUsecaseDto {
+  techName: string;
+  techCategoryId: number;
+  voyageTeamMemberId: number;
+}
