11a54e329e2b86d9e5a0063ba522d266d54797f5
Dan Ko <danko56666@gmail.com>
2025-04-05 23:08:28 -0400

    chore: add FetchTechStackUsecaseDto

---
+++ src/tech-stack/adapters/secondary/tech-stack-api-adapter.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from "tsyringe";
+import { TYPES } from "@/types";
+import { RestApiPort } from "@/rest-api/ports/secondary/rest-api-port";
+import { TechStackApiPort } from "@/tech-stack/ports/secondary/tech-stack-api-port";
+import { FetchTechStackApiRequestDto } from "@/tech-stack/application/dtos/request.dto";
+import { FetchTechStackResponseDto } from "@/tech-stack/application/dtos/response.dto";
+
+@injectable()
+export class TechStackApiAdapter implements TechStackApiPort {
+  constructor(
+    @inject(TYPES.RestApiPort)
+    private readonly apiClient: RestApiPort,
+  ) {}
+
+  async fetchTechStack({
+    teamId,
+  }: FetchTechStackApiRequestDto): Promise<FetchTechStackResponseDto> {
+    return await this.apiClient.get({
+      url: UserUrls.fetchUser(),
+    });
+  }
+}
---
+++ src/tech-stack/application/dtos/usecase.dto.ts
@@ -0,0 +1,3 @@
+export interface FetchTechStackUsecaseDto {
+  teamId: string;
+}
