0b82ffefcd34ae1cc790fd452a1eb16ec8f030ea
Dan Ko <danko56666@gmail.com>
2025-04-05 23:38:21 -0400

    chore: add AddTechStackItemResponseDto

--- src/tech-stack/application/dtos/response.dto.ts
+++ src/tech-stack/application/dtos/response.dto.ts
@@ -1,3 +1,11 @@
 import type { TechStackCategory } from "@/tech-stack/application/types";
 
 export type FetchTechStackResponseDto = TechStackCategory[];
+
+export interface AddTechStackItemResponseDto {
+  teamTechStackItemVoteId: number;
+  teamTechId: number;
+  teamMemberId: number;
+  createdAt: string;
+  updatedAt: string;
+}
