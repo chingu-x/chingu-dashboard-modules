3fcd92e27050400b762a965a6f275892971c0fd4
Dan Ko <danko56666@gmail.com>
2025-04-06 00:01:27 -0400

    chore: add editTechStackItem url

--- src/tech-stack/application/constants/tech-stack-urls.ts
+++ src/tech-stack/application/constants/tech-stack-urls.ts
@@ -6,12 +6,19 @@ interface AddTechStackItemUrlProps {
   teamId: string;
 }
 
+interface EditTechStackItemUrlProps {
+  teamTechItemId: number;
+}
+
 const TechStackUrls = {
   fetchTechStack({ teamId }: FetchTechStackUrlProps) {
     return `/api/v1/voyages/teams/${teamId}/techs`;
   },
   addTechStackItem({ teamId }: AddTechStackItemUrlProps) {
     return `/api/v1/voyages/teams/${teamId}/techs`;
+  },
+  editTechStackItem({ teamTechItemId }: EditTechStackItemUrlProps) {
+    return `/api/v1/voyages/techs/${teamTechItemId}`;
   },
 } as const;
 
