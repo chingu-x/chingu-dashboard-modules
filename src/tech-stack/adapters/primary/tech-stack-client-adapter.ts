ce67b378ba192f78222859659f09b3a4cf9ba1e5
Dan Ko <danko56666@gmail.com>
2025-04-05 23:40:02 -0400

    feat: implement addTechStackItem function in TechStackClientAdapter

--- src/tech-stack/adapters/primary/tech-stack-client-adapter.ts
+++ src/tech-stack/adapters/primary/tech-stack-client-adapter.ts
@@ -1,8 +1,14 @@
 import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { TechStackClientPort } from "@/tech-stack/ports/primary/tech-stack-client-port";
-import { FetchTechStackClientRequestDto } from "@/tech-stack/application/dtos/request.dto";
-import { FetchTechStackResponseDto } from "@/tech-stack/application/dtos/response.dto";
+import {
+  AddTechStackItemClientRequestDto,
+  FetchTechStackClientRequestDto,
+} from "@/tech-stack/application/dtos/request.dto";
+import {
+  AddTechStackItemResponseDto,
+  FetchTechStackResponseDto,
+} from "@/tech-stack/application/dtos/response.dto";
 import { FetchTechStackUsecase } from "@/tech-stack/application/usecases/fetch-tech-stack-usecase";
 
 @injectable()
@@ -10,11 +16,26 @@ export class TechStackClientAdapter implements TechStackClientPort {
   constructor(
     @inject(TYPES.FetchTechStackUsecase)
     private readonly fetchTechStackUsecase: FetchTechStackUsecase,
+
+    @inject(TYPES.AddTechStackItemUsecase)
+    private readonly addTechStackItemUsecase: AddTechStackItemUsecase,
   ) {}
 
   async fetchTechStack({
     teamId,
   }: FetchTechStackClientRequestDto): Promise<FetchTechStackResponseDto> {
     return await this.fetchTechStackUsecase.execute({ teamId });
+  }
+
+  async addTechStackItem({
+    techName,
+    techCategoryId,
+    voyageTeamMemberId,
+  }: AddTechStackItemClientRequestDto): Promise<AddTechStackItemResponseDto> {
+    return await this.addTechStackItemUsecase.execute({
+      techName,
+      techCategoryId,
+      voyageTeamMemberId,
+    });
   }
 }
