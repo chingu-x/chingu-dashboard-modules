62bb1edc0344ed1e450202c2d7e46d736477c079
Dan Ko <danko56666@gmail.com>
2025-04-05 23:04:30 -0400

    chore: update imports

--- src/tech-stack/ports/secondary/tech-stack-api-port.ts
+++ src/tech-stack/ports/secondary/tech-stack-api-port.ts
@@ -1,3 +1,6 @@
+import type { FetchTechStackApiRequestDto } from "@/tech-stack/application/dtos/request.dto";
+import type { FetchTechStackResponseDto } from "@/tech-stack/application/dtos/response.dto";
+
 export interface TechStackApiPort {
   fetchTechStack: ({
     teamId,
