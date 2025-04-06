fc759f35c28737409e4e097103ada404512659c3
Dan Ko <danko56666@gmail.com>
2025-04-06 00:19:24 -0400

    chore: add deleteTechStackItem url

--- src/tech-stack/application/constants/tech-stack-urls.ts
+++ src/tech-stack/application/constants/tech-stack-urls.ts
@@ -10,6 +10,10 @@ interface EditTechStackItemUrlProps {
   teamTechItemId: number;
 }
 
+interface DeleteTechStackItemUrlProps {
+  teamTechItemId: number;
+}
+
 const TechStackUrls = {
   fetchTechStack({ teamId }: FetchTechStackUrlProps) {
     return `/api/v1/voyages/teams/${teamId}/techs`;
@@ -18,6 +22,9 @@ const TechStackUrls = {
     return `/api/v1/voyages/teams/${teamId}/techs`;
   },
   editTechStackItem({ teamTechItemId }: EditTechStackItemUrlProps) {
+    return `/api/v1/voyages/techs/${teamTechItemId}`;
+  },
+  deleteTechStackItem({ teamTechItemId }: DeleteTechStackItemUrlProps) {
     return `/api/v1/voyages/techs/${teamTechItemId}`;
   },
 } as const;
