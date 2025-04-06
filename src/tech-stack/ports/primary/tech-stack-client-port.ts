96c42d96cb4c89452e3a03d02cc9c3782c0ec879
Dan Ko <danko56666@gmail.com>
2025-04-05 23:57:40 -0400

    chore: update imports

--- src/tech-stack/ports/primary/tech-stack-client-port.ts
+++ src/tech-stack/ports/primary/tech-stack-client-port.ts
@@ -1,9 +1,11 @@
 import type {
   AddTechStackItemClientRequestDto,
+  EditTechStackItemClientRequestDto,
   FetchTechStackClientRequestDto,
 } from "@/tech-stack/application/dtos/request.dto";
 import type {
   AddTechStackItemResponseDto,
+  EditTechStackItemResponseDto,
   FetchTechStackResponseDto,
 } from "@/tech-stack/application/dtos/response.dto";
 
