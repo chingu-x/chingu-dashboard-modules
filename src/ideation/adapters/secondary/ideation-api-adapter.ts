0a582707853acc4f5d0f73c32a9d5163377ed8e5
Dan Ko <danko56666@gmail.com>
2025-04-09 00:47:35 -0400

    feat: implement fetchIdeation function in IdeationApiAdapter

---
+++ src/ideation/adapters/secondary/ideation-api-adapter.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from "tsyringe";
+import { TYPES } from "@/types";
+import { RestApiPort } from "@/rest-api/ports/secondary/rest-api-port";
+import { IdeationApiPort } from "@/ideation/ports/secondary/ideation-api-port";
+import { FetchIdeationApiRequestDto } from "@/ideation/application/dtos/request.dto";
+import { FetchIdeationClientResponseDto } from "@/ideation/application/dtos/response.dto";
+import IdeationUrls from "@/ideation/application/constants/ideation-urls";
+
+@injectable()
+export class IdeationApiAdapter implements IdeationApiPort {
+  constructor(
+    @inject(TYPES.RestApiPort)
+    private readonly apiClient: RestApiPort,
+  ) {}
+
+  async fetchIdeation({
+    teamId,
+  }: FetchIdeationApiRequestDto): Promise<FetchIdeationClientResponseDto> {
+    return await this.apiClient.get({
+      url: IdeationUrls.fetchIdeation({ teamId }),
+    });
+  }
+}
