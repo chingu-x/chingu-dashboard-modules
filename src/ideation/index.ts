16dae2a06c6fada22b3d1360accba28849c54886
Dan Ko <danko56666@gmail.com>
2025-04-15 10:57:17 -0400

    chore: update exports

--- src/ideation/index.ts
+++ src/ideation/index.ts
@@ -5,11 +5,13 @@ export {
   EditIdeationClientRequestDto,
   DeleteIdeationClientRequestDto,
   AddIdeationVoteClientRequestDto,
+  RemoveIdeationVoteClientRequestDto,
 } from "./application/dtos/request.dto";
 export {
   AddIdeationResponseDto,
   EditIdeationResponseDto,
   DeleteIdeationResponseDto,
   AddIdeationVoteResponseDto,
+  RemoveIdeationVoteResponseDto,
 } from "./application/dtos/response.dto";
 export { Ideation, ProjectIdeaVotes } from "./application/types";
