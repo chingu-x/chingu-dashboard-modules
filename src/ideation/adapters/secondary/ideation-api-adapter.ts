5fa150a9fc6ceed995f8f4ac034323c21846295f
Dan Ko <danko56666@gmail.com>
2025-04-15 10:03:14 -0400

    feat: implement addIdeation function in IdeationApiAdapter

--- src/ideation/adapters/secondary/ideation-api-adapter.ts
+++ src/ideation/adapters/secondary/ideation-api-adapter.ts
@@ -2,8 +2,14 @@ import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { RestApiPort } from "@/rest-api/ports/secondary/rest-api-port";
 import { IdeationApiPort } from "@/ideation/ports/secondary/ideation-api-port";
-import { FetchIdeationApiRequestDto } from "@/ideation/application/dtos/request.dto";
-import { FetchIdeationResponseDto } from "@/ideation/application/dtos/response.dto";
+import {
+  AddIdeationApiRequestDto,
+  FetchIdeationApiRequestDto,
+} from "@/ideation/application/dtos/request.dto";
+import {
+  AddIdeationResponseDto,
+  FetchIdeationResponseDto,
+} from "@/ideation/application/dtos/response.dto";
 import IdeationUrls from "@/ideation/application/constants/ideation-urls";
 
 @injectable()
@@ -18,6 +24,18 @@ export class IdeationApiAdapter implements IdeationApiPort {
   }: FetchIdeationApiRequestDto): Promise<FetchIdeationResponseDto> {
     return await this.apiClient.get({
       url: IdeationUrls.fetchIdeation({ teamId }),
+    });
+  }
+
+  async addIdeation({
+    teamId,
+    title,
+    description,
+    vision,
+  }: AddIdeationApiRequestDto): Promise<AddIdeationResponseDto> {
+    return await this.apiClient.post({
+      url: IdeationUrls.addIdeation({ teamId }),
+      payload: { title, description, vision },
     });
   }
 }
