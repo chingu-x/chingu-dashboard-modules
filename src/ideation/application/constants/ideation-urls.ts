a491d03caf67a0c3ccbe2d14bc42f89ed485aa91
Dan Ko <danko56666@gmail.com>
2025-04-15 10:24:59 -0400

    chore: add deleteIdeation url

--- src/ideation/application/constants/ideation-urls.ts
+++ src/ideation/application/constants/ideation-urls.ts
@@ -10,6 +10,10 @@ interface EditIdeationUrlProps {
   ideationId: number;
 }
 
+interface DeleteIdeationUrlProps {
+  ideationId: number;
+}
+
 const IdeationUrls = {
   fetchIdeation({ teamId }: FetchIdeationUrlProps) {
     return `/api/v1/voyages/teams/${teamId}/ideations`;
@@ -18,6 +22,9 @@ const IdeationUrls = {
     return `/api/v1/voyages/teams/${teamId}/ideations`;
   },
   editIdeation({ ideationId }: EditIdeationUrlProps) {
+    return `/api/v1/voyages/ideations/${ideationId}`;
+  },
+  deleteIdeation({ ideationId }: DeleteIdeationUrlProps) {
     return `/api/v1/voyages/ideations/${ideationId}`;
   },
 } as const;
