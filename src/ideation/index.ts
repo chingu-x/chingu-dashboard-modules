88808275e8cd165bd5e9b1d8bcfafd6f26c8631d
Dan Ko <danko56666@gmail.com>
2025-04-15 10:06:02 -0400

    chore: update exports

--- src/ideation/index.ts
+++ src/ideation/index.ts
@@ -1,3 +1,7 @@
 export { IdeationClientAdapter } from "./adapters/primary/ideation-client-adapter";
-export { FetchIdeationClientRequestDto } from "./application/dtos/request.dto";
+export {
+  FetchIdeationClientRequestDto,
+  AddIdeationClientRequestDto,
+} from "./application/dtos/request.dto";
+export { AddIdeationResponseDto } from "./application/dtos/response.dto";
 export { Ideation, ProjectIdeaVotes } from "./application/types";
