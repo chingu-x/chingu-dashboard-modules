2ed248482d976693446dc92f841421653fe5e58e
Dan Ko <danko56666@gmail.com>
2025-04-15 09:56:02 -0400

    chore: add AddIdeationClientResponseDto

--- src/ideation/application/dtos/response.dto.ts
+++ src/ideation/application/dtos/response.dto.ts
@@ -1,3 +1,14 @@
 import type { Ideation } from "@/ideation/application/types";
 
 export type FetchIdeationClientResponseDto = Ideation[];
+
+export interface AddIdeationClientResponseDto {
+  id: number;
+  voyageTeamMemberId: number;
+  title: string;
+  description: string;
+  vision: string;
+  isSelected: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
