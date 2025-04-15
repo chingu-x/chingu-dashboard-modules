cd2869e12a29f25849f4950cfc36b58f48d874eb
Dan Ko <danko56666@gmail.com>
2025-04-15 09:54:27 -0400

    chore: add AddIdeationClientRequestDto

--- src/ideation/application/dtos/request.dto.ts
+++ src/ideation/application/dtos/request.dto.ts
@@ -3,6 +3,13 @@ export interface FetchIdeationClientRequestDto {
   teamId: string;
 }
 
+export interface AddIdeationClientRequestDto {
+  teamId: string;
+  title: string;
+  description: string;
+  vision: string;
+}
+
 // api
 export interface FetchIdeationApiRequestDto {
   teamId: string;
