d59f7d05ff9ad65f0c2947a0c8470b4c0874cbdd
Dan Ko <danko56666@gmail.com>
2025-04-15 10:12:28 -0400

    chore: add editIdeation url

--- src/ideation/application/constants/ideation-urls.ts
+++ src/ideation/application/constants/ideation-urls.ts
@@ -6,12 +6,19 @@ interface AddIdeationUrlProps {
   teamId: string;
 }
 
+interface EditIdeationUrlProps {
+  ideationId: number;
+}
+
 const IdeationUrls = {
   fetchIdeation({ teamId }: FetchIdeationUrlProps) {
     return `/api/v1/voyages/teams/${teamId}/ideations`;
   },
   addIdeation({ teamId }: AddIdeationUrlProps) {
     return `/api/v1/voyages/teams/${teamId}/ideations`;
+  },
+  editIdeation({ ideationId }: EditIdeationUrlProps) {
+    return `/api/v1/voyages/ideations/${ideationId}`;
   },
 } as const;
 
