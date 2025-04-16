ff8224f03eb91e07848ec79e1bc8b06c2faa7a8a
Dan Ko <danko56666@gmail.com>
2025-04-15 20:02:41 -0400

    chore: add getTopFiveResources function in VoyageResourcesClientPort

--- src/voyage-resources/ports/primary/voyage-resources-client-port.ts
+++ src/voyage-resources/ports/primary/voyage-resources-client-port.ts
@@ -27,4 +27,8 @@ export interface VoyageResourcesClientPort {
   sortVoyageResources: (
     props: SortVoyageResourcesClientRequestDto,
   ) => SortVoyageResourcesResponseDto;
+
+  getTopFiveResources: (
+    props: GetTopFiveResourcesClientRequestDto,
+  ) => GetTopFiveResourcesResponseDto;
 }
