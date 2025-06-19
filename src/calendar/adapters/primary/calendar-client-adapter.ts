a6d2e9a53049f4db6eb04c97c084681554bb443d
Dan Ko <danko56666@gmail.com>
2025-06-18 21:23:44 -0400

    feat: implement getVoyageEndDate function in calendar client adapter

--- src/calendar/adapters/primary/calendar-client-adapter.ts
+++ src/calendar/adapters/primary/calendar-client-adapter.ts
@@ -1,8 +1,14 @@
 import { inject, injectable } from "tsyringe";
 import { TYPES } from "@/types";
 import { CalendarClientPort } from "@/calendar/ports/primary/calendar-client-port";
-import { GetVoyageStartDateClientRequestDto } from "@/calendar/application/dtos/request.dto";
-import { GetVoyageStartDateResponseDto } from "@/calendar/application/dtos/response.dto";
+import {
+  GetVoyageEndDateClientRequestDto,
+  GetVoyageStartDateClientRequestDto,
+} from "@/calendar/application/dtos/request.dto";
+import {
+  GetVoyageEndDateResponseDto,
+  GetVoyageStartDateResponseDto,
+} from "@/calendar/application/dtos/response.dto";
 import { GetVoyageStartDateUsecase } from "@/calendar/application/usecases/get-voyage-start-data-usecase";
 
 @injectable()
@@ -10,11 +16,20 @@ export class CalendarClientAdapter implements CalendarClientPort {
   constructor(
     @inject(TYPES.GetVoyageStartDateUsecase)
     private readonly getVoyageStartDateUsecase: GetVoyageStartDateUsecase,
+
+    @inject(TYPES.GetVoyageEndDateUsecase)
+    private readonly getVoyageEndDateUsecase: GetVoyageEndDateUsecase,
   ) {}
 
   getVoyageStartDate({
     sprintsData,
   }: GetVoyageStartDateClientRequestDto): GetVoyageStartDateResponseDto {
     return this.getVoyageStartDateUsecase.execute({ sprintsData });
+  }
+
+  getVoyageEndDate({
+    sprintsData,
+  }: GetVoyageEndDateClientRequestDto): GetVoyageEndDateResponseDto {
+    return this.getVoyageEndDateUsecase.execute({ sprintsData });
   }
 }
