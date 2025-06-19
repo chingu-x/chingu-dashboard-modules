17224c527e2d6754d7dcecc8deb98053a000a089
Dan Ko <danko56666@gmail.com>
2025-06-18 20:56:18 -0400

    feat: add calendar client adapter

---
+++ src/calendar/adapters/primary/calendar-client-adapter.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from "tsyringe";
+import { TYPES } from "@/types";
+import { CalendarClientPort } from "@/calendar/ports/primary/calendar-client-port";
+import { GetVoyageStartDateClientRequestDto } from "@/calendar/application/dtos/request.dto";
+import { GetVoyageStartDateResponseDto } from "@/calendar/application/dtos/response.dto";
+
+@injectable()
+export class CalendarClientAdapter implements CalendarClientPort {
+  constructor(
+    @inject(TYPES.GetVoyageStartDateUsecase)
+    private readonly getVoyageStartDateUsecase: GetVoyageStartDateUsecase,
+  ) {}
+
+  getVoyageStartDate({
+    sprintsData,
+  }: GetVoyageStartDateClientRequestDto): GetVoyageStartDateResponseDto {
+    return this.getVoyageStartDateUsecase.execute({ sprintsData });
+  }
+}
