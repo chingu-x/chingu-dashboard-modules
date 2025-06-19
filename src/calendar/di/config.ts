8ddc0040cec1f5de8945eea06504eb4ad96aa678
Dan Ko <danko56666@gmail.com>
2025-06-18 21:27:20 -0400

    chore: update calendar di container

--- src/calendar/di/config.ts
+++ src/calendar/di/config.ts
@@ -2,6 +2,7 @@ import { container } from "tsyringe";
 import { CALENDAR_TYPES } from "./types";
 import { GetVoyageStartDateUsecase } from "@/calendar/application/usecases/get-voyage-start-date-usecase";
 import { CalendarClientAdapter } from "@/calendar/adapters/primary/calendar-client-adapter";
+import { GetVoyageEndDateUsecase } from "@/calendar/application/usecases/get-voyage-end-date-usecase";
 
 export const registerCalendarDependencies = () => {
   // ports
@@ -9,6 +10,10 @@ export const registerCalendarDependencies = () => {
   // usecases
   container.register(CALENDAR_TYPES.GetVoyageStartDateUsecase, {
     useClass: GetVoyageStartDateUsecase,
+  });
+
+  container.register(CALENDAR_TYPES.GetVoyageEndDateUsecase, {
+    useClass: GetVoyageEndDateUsecase,
   });
 
   // adapters
--- src/calendar/di/types.ts
+++ src/calendar/di/types.ts
@@ -1,6 +1,7 @@
 import type { InjectionToken } from "tsyringe";
 import type { CalendarClientAdapter } from "@/calendar/adapters/primary/calendar-client-adapter";
 import type { GetVoyageStartDateUsecase } from "@/calendar/application/usecases/get-voyage-start-date-usecase";
+import type { GetVoyageEndDateUsecase } from "@/calendar/application/usecases/get-voyage-end-date-usecase";
 
 export const CALENDAR_TYPES = {
   // ports
@@ -9,6 +10,10 @@ export const CALENDAR_TYPES = {
   GetVoyageStartDateUsecase: Symbol.for(
     "GetVoyageStartDateUsecase",
   ) as InjectionToken<GetVoyageStartDateUsecase>,
+
+  GetVoyageEndDateUsecase: Symbol.for(
+    "GetVoyageEndDateUsecase",
+  ) as InjectionToken<GetVoyageEndDateUsecase>,
 
   // adapters
   CalendarClientAdapter: Symbol.for(
