9f8eb2b0b04e03c10df0cd0a6bb2877a3539bbce
Dan Ko <danko56666@gmail.com>
2025-06-18 21:24:13 -0400

    chore: add GetVoyageEndDateUsecaseDto

--- src/calendar/application/dtos/usecase.dto.ts
+++ src/calendar/application/dtos/usecase.dto.ts
@@ -3,3 +3,7 @@ import type { Sprint } from "@/sprints/application/types";
 export interface GetVoyageStartDateUsecaseDto {
   sprintsData: Sprint[];
 }
+
+export interface GetVoyageEndDateUsecaseDto {
+  sprintsData: Sprint[];
+}
