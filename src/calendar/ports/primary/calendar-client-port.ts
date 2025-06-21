93a45175df6272a58ba5fdbf5e8ce3d4b22f0bf2
Dan Ko <danko56666@gmail.com>
2025-06-21 17:17:55 -0400

    chore: add isWithinTwoDates function in calendar client port

--- src/calendar/ports/primary/calendar-client-port.ts
+++ src/calendar/ports/primary/calendar-client-port.ts
@@ -1,10 +1,12 @@
 import type {
   GetVoyageEndDateClientRequestDto,
   GetVoyageStartDateClientRequestDto,
+  IsWithinTwoDatesClientRequestDto,
 } from "@/calendar/application/dtos/request.dto";
 import type {
   GetVoyageEndDateResponseDto,
   GetVoyageStartDateResponseDto,
+  IsWithinTwoDatesResponseDto,
 } from "@/calendar/application/dtos/response.dto";
 
 export interface CalendarClientPort {
