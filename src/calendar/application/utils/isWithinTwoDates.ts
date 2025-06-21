46ce1c71bc3493f3df76c9cecbf65f6b7f7ad448
Dan Ko <danko56666@gmail.com>
2025-06-21 17:23:04 -0400

    feat: implement isWithinTwoDates utility function

---
+++ src/calendar/application/utils/isWithinTwoDates.ts
@@ -0,0 +1,18 @@
+import { isSameDay } from "date-fns";
+
+interface IsWithinTwoDatesProps {
+  currentDate: Date | string;
+  startDate: Date | string;
+  endDate: Date | string;
+}
+
+export function isWithinTwoDates({
+  currentDate,
+  startDate,
+  endDate,
+}: IsWithinTwoDatesProps) {
+  return (
+    (isSameDay(currentDate, startDate) || currentDate >= startDate) &&
+    (isSameDay(currentDate, endDate) || currentDate <= endDate)
+  );
+}
