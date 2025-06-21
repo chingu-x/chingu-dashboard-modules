3185e3ad16875590381a35e3b0d21e6f8c1c49f5
Dan Ko <danko56666@gmail.com>
2025-06-21 17:26:30 -0400

    chore: add IsWithinSprintRangeResponseDto

--- src/calendar/application/dtos/response.dto.ts
+++ src/calendar/application/dtos/response.dto.ts
@@ -1,3 +1,5 @@
 export type GetVoyageStartDateResponseDto = string | undefined;
 
 export type GetVoyageEndDateResponseDto = string | undefined;
+
+export type IsWithinSprintRangeResponseDto = boolean;
