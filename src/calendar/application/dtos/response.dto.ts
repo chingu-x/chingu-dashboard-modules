2cabb03651fda4ccef71991d22828ce0c7556ea3
Dan Ko <danko56666@gmail.com>
2025-06-24 19:43:59 -0400

    chore: add GetSelectedSprintResponseDto

--- src/calendar/application/dtos/response.dto.ts
+++ src/calendar/application/dtos/response.dto.ts
@@ -3,3 +3,5 @@ export type GetVoyageStartDateResponseDto = string | undefined;
 export type GetVoyageEndDateResponseDto = string | undefined;
 
 export type IsWithinSprintRangeResponseDto = boolean;
+
+export type GetSelectedSprintResponseDto = number;
