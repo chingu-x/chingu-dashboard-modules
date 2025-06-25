9c605fb69cecd4198b46b879fecc650d7ea91ad6
Dan Ko <danko56666@gmail.com>
2025-06-24 20:21:17 -0400

    chore: add GetMeetingEventDataResponseDto

--- src/calendar/application/dtos/response.dto.ts
+++ src/calendar/application/dtos/response.dto.ts
@@ -1,3 +1,5 @@
+import type { Meeting } from "@/sprint-meeting/application/types";
+
 export type GetVoyageStartDateResponseDto = string | undefined;
 
 export type GetVoyageEndDateResponseDto = string | undefined;
@@ -5,3 +7,5 @@ export type GetVoyageEndDateResponseDto = string | undefined;
 export type IsWithinSprintRangeResponseDto = boolean;
 
 export type GetSelectedSprintResponseDto = number | undefined;
+
+export type GetMeetingEventDataResponseDto = Meeting;
